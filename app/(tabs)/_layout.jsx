89a53699eb2bc60aaf1320905cdb817f4cc6c412
Quyền Đặng <ninicom206@gmail.com>
2024-11-27 16:59:33 +0700

    uppdate with no error

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -20,10 +20,9 @@ const renderScene = SceneMap({
   testapi: TestAPI,
 });
 
-const renderTabBar = ({ key, ...restProps }) => (
+const renderTabBar = props => (
   <TabBar
-    key={key}
-    {...restProps}
+    {...props}
     renderIcon={({ route, focused, color }) => (
       <Image
         source={route.icon}
@@ -44,29 +43,29 @@ export default function TabViewExample() {
   const layout = useWindowDimensions();
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    { key: 'testapi', icon: icon.bookmark },
     { key: 'home', icon: icon.home },
     { key: 'chat', icon: icon.chat },
     { key: 'create', icon: icon.plus },
     { key: 'friend', icon: icon.friends },
-    { key: 'profile', icon: icon.profile }
+    { key: 'profile', icon: icon.profile },
+    { key: 'testapi', icon: icon.bookmark },
   ]);
 
   return (
     <SafeAreaView className='h-full'>
-      <StatusBar 
-        backgroundColor='#FFFFFF'
-        barStyle='dark-content'
-        hidden={false}
-      />
-      <TabView
-        navigationState={{ index, routes }}
-        renderScene={renderScene}
-        renderTabBar={renderTabBar}
-        onIndexChange={setIndex}
-        initialLayout={{ width: layout.width }}      
-        tabBarPosition='bottom'              
-      />
+        <StatusBar 
+                backgroundColor='#FFFFFF'
+                barStyle='dark-content'
+                hidden={false}
+        />
+        <TabView
+            navigationState={{ index, routes }}
+            renderScene={renderScene}
+            renderTabBar={renderTabBar}
+            onIndexChange={setIndex}
+            initialLayout={{ width: layout.width }}      
+            tabBarPosition='bottom'              
+        />
     </SafeAreaView>
   );
 }
